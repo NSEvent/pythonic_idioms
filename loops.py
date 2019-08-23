caee3625d28d9f04f6357079f1e3bbcd044cfada
Kevin Tang <44446865+kvntng17@users.noreply.github.com>
2019-08-23 12:21:07 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -7,16 +7,32 @@ for i in range(5):
 # Reverse for each loop 
 for i in reversed(range(5)):
 	print(i)
+###################################################
 
+# Idiomatic examples to traverse lists
+numbers = ['zero', 'one', 'two', 'three', 'four']
 
-# Idiomatic examples to traverse numbers
- numbers = ['zero', 'one', 'two', 'three', 'four']
-
+# Traverse in order
 for num in numbers:
 	print(num)
-
+	
+# Traverse in reverse order
 for num in reversed(numbers):
 	print(num)
-
+	
+# Traverse in order with indices
 for i, num in enumerate(numbers):
 	print(f'{i}->{num}')
+	
+# Traverse n lists in order in parallel up to the len of the shortest list
+colors = ['red', 'green', 'blue']
+for num, color in zip(numbers, colors):
+	print(f'{num}->{color}')
+	
+# Traverse in sorted order
+for num in sorted(numbers):
+	print(num)
+
+# Traverse in key sorted order
+for num in sorted(numbers, key=len):
+	print(f'{num} (len={len(num)})')
